62847e0c40011ab46c78d21ac51a392ec01fbaaf
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-23 17:48:03 +0800

    Create twosum.c

---
+++ Training/twosum.c
@@ -0,0 +1,16 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+    int *arr = malloc(sizeof(int) * 2);
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i+1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                arr[0] = i; arr[1] = j;
+            }
+        }
+    }
+
+    *returnSize = 2;
+    
+    return arr;
+}
