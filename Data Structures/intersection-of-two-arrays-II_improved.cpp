2a854c4efcc8c6d8e5db828c4373ca33feffbc0d
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-26 16:40:37 +0800

    Update intersection-of-two-arrays-II_improved.cpp

--- Data Structures/intersection-of-two-arrays-II_improved.cpp
+++ Data Structures/intersection-of-two-arrays-II_improved.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         
-    if (!nums1.size() || !nums2.size()) return vector<int>();
+    if (nums1.size() == 0 || nums2.size() == 0) return vector<int>();
         
     unordered_map<long,int> map;
     vector<int> res;
 
-    for (int i=0; i<nums1.size(); i++) map[nums1[i]]++;
+    for (int i = 0; i < nums1.size(); i++) map[nums1[i]]++;
         
-    for (int i=0; i < nums2.size();i++) {
-        if (map[nums2[i]]>0) {
+    for (int i = 0; i < nums2.size(); i++) {
+        if (map[nums2[i]] > 0) {
             res.push_back(nums2[i]);
             map[nums2[i]]--;
         }
