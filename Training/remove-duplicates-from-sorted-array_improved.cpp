361d74634674e9db8186a472c32bf47325242110
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-30 13:27:22 +0800

    Update remove-duplicates-from-sorted-array_improved.cpp

--- Training/remove-duplicates-from-sorted-array_improved.cpp
+++ Training/remove-duplicates-from-sorted-array_improved.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
+        if (nums.size() < 2) return nums.size();
         int flag = nums[0];
         int count = 1;
         for (int i = 1; i < nums.size(); i++) {
@@ -10,6 +10,6 @@ public:
                 flag = nums[i];
             }
         } 
-    return count;
+        return count;
     }
 };
