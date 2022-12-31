1421b4168b6a8d26e382e8466bc23d7c3a36a3c7
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-31 14:50:30 +0800

    Update search-a-2d-matrix.cpp

--- Data Structures/Arrays/search-a-2d-matrix.cpp
+++ Data Structures/Arrays/search-a-2d-matrix.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size(),
-	cols = matrix[0].size(),
-        row = 0, col = cols - 1;
+	    cols = matrix[0].size(),
+            row = 0, col = cols - 1;
         while (row < rows && col > -1) {
             int cur = matrix[row][col];
             if (cur == target) return true;
