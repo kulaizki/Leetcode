c9e4255d10e1a461fd54735bca74cc56c75d5941
kulaizki <91787757+kulaizki@users.noreply.github.com>
2023-01-07 11:12:58 +0800

    Update ransom-note.cpp

--- Solutions/ransom-note.cpp
+++ Solutions/ransom-note.cpp
@@ -3,12 +3,12 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int counts[26] = {0};
+        int count[26] = {0};
         for (int i = 0; i < magazine.size(); i++)
-            counts[magazine[i] - 'a']++;
+            count[magazine[i] - 'a']++;
         for (int i = 0; i < ransomNote.size(); i++) {
-            counts[ransomNote[i] - 'a']--;
-            if (counts[ransomNote[i] - 'a'] < 0)
+            count[ransomNote[i] - 'a']--;
+            if (count[ransomNote[i] - 'a'] < 0)
                 return false;
         }
         return true;
