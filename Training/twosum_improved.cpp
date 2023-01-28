67fee153c75bfab4787462bad69fee02a29debd0
Fitz Angelo <91787757+kulaizki@users.noreply.github.com>
2023-01-28 14:04:47 +0800

    format

--- Training/twosum_improved.cpp
+++ Training/twosum_improved.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-	    unordered_map<int, int> hash; //Number, Index       
+	    unordered_map<int, int> hash; // Number, Index       
 	    for (int i = 0; i < numbers.size(); i++) {
 		    int numberToFind = target - numbers[i];
 		    if (hash.find(numberToFind) != hash.end()) 
