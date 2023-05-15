988637d73c359a9596947045d9c8e4b799e0388f
Fitz Angelo <91787757+kulaizki@users.noreply.github.com>
2023-05-15 23:59:45 +0800

    Update search-insert-position.py
    binary search, multiple assignment

--- Training/search-insert-position.py
+++ Training/search-insert-position.py
@@ -1,8 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
 
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
